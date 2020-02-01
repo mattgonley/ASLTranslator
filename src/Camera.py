b10508044c963cc4309ca1b8472c1c2b94febd50
Matt Gonley <50349733+mattgonley@users.noreply.github.com>
2020-02-01 09:08:31 -0500

    ''

--- src/Camera.py
+++ src/Camera.py
@@ -27,13 +27,16 @@ while (True):
             if frame is None:
                 break
             fgMask = backSub.apply(frame)
-            cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
-            cv.putText(frame, str(cap.get(cv.CAP_PROP_POS_FRAMES)),
-                       (15, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0,))
+            #cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
+            #cv.putText(frame, str(cap.get(cv.CAP_PROP_POS_FRAMES)),
+            #           (15, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0,))
             #shows the coloured frame
             cv.imshow('Frame', frame)
             #shows the masked frame
             cv.imshow('FG Mask', fgMask)
+            # captures the frames
+            cv.imwrite('frame.jpg', frame)
+
             keyboard = cv.waitKey(27)
             if keyboard == 27:
                 exit(0)
