b080f4f9d23c2a9a6de66b668a8f8b0b0b331a06
Marcus Tran <46179752+Vysiar@users.noreply.github.com>
2020-01-31 21:21:21 -0500

    figuring the window

--- src/GUI.py
+++ src/GUI.py
@@ -1,4 +1,44 @@
-import tkinter
-import tkinter.ttk
-import cv2 as cv
+""""
+:author: Ryan Nicholas, Matt Gonley, Marcus Tran, Nicole Fitz
+:date: 1/31/2020
+:description: Main file for the GUI, contains the window, camera input,
+ and display of translated text
+"""
+from __future__ import print_function
+from PIL import Image
+from PIL import ImageTk
+from tkinter import *
+import threading
+import datetime
+import imutils
+import cv2
+import os
 
+
+class Window:
+
+    def __init__(self, width, height):
+        """
+
+        :param width: x "dimension" of the window,  pixels for the width
+        :param height: y "dimension" of the window, pixels for the height of the window
+        """
+        
+        self.root = Tk()
+        self.thread = None
+        self.stopEvent = None
+        self.root.title("Sign Language Translator")
+        geometry = "%dx%d" % (width, height)
+        self.root.geometry(geometry)
+        self.root.mainloop()
+
+class TextBox:
+    def __init__(self, window):
+
+        scrollbar = Scrollbar(window)
+        text = Text(window, height=50, width=500, yscrollcommand=scrollbar.set)
+        text.pack(side=window.RIGHT)
+        scrollbar.pack(side=RIGHT, fill=window.Y)
+        text.tag_configure('size', font=('Times New Roman', 14))
+        test = "Hello, World"
+        text.insert(test, 'size')
